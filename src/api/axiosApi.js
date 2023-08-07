675d3c880c088b6a557401af88599305bdeaa0e0
JohanDev <johandev225@gmail.com>
2023-08-07 12:36:59 -0500

    with url

--- src/api/axiosApi.js
+++ src/api/axiosApi.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 const base = import.meta.env.VITE_BASE_URL;
-const baseDev = import.meta.env.VITE_BASE_DEV_URL;
+//const baseDev = import.meta.env.VITE_BASE_DEV_URL;
 
 
 export const backApi = axios.create({
-    baseURL: baseDev,
+    baseURL: base,
 });
